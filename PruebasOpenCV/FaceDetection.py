9339c20359fc729f71ac61bd5576e145bf5da6a0
Arkh3 <ateruelfe@gmail.com>
2021-11-08 18:40:08 +0100

    FaceDetection file+ Imagenes de test incluidas

--- PruebasOpenCV/FaceDetection.py
+++ PruebasOpenCV/FaceDetection.py
@@ -4,13 +4,6 @@ import os, os.path
 import numpy as np
 
 
-user_id = "default" # pedir en algún momento el identificador
-
-modelo = 'LBPH'
-
-faceDimentions = (260,260)
-
-
 def loadCascade(cascade):
 
     cascadePath = cv.data.haarcascades + cascade
@@ -24,7 +17,7 @@ def loadCascade(cascade):
     return face_cascade
 
 
-def train(images, labels):
+def train(images, labels, modelo, user):
 
     if modelo == 'EigenFaces': recognizer = cv.face.EigenFaceRecognizer_create()
     if modelo == 'FisherFaces': recognizer = cv.face.FisherFaceRecognizer_create()
@@ -38,18 +31,18 @@ def train(images, labels):
 
     print("El reconocedor ha terminado entrenarse!")
 
-    path = 'users/' + user_id + "/recognizer/"
+    path = 'users/' + user + "/recognizer/"
     
     # se guardan los datos del training en el archivo correspondiente
     recognizer.save(path + "trainer.yml")
 
 
-def getTrainingInfo():
+def getTrainingInfo(user):
 
     images = []
     labels = [] # Think of the label as the subject (the person) this image belongs to, so same subjects (persons) should have the same label. 
 
-    path = "users/" + user_id + "/train/"
+    path = "users/" + user + "/train/"
 
     for img in os.listdir(path):
 
@@ -63,25 +56,27 @@ def getTrainingInfo():
     return images, labels 
 
 
-def recognize(id, face):
+def recognize(id, face, modelo, user):
 
     if modelo == 'EigenFaces': recognizer = cv.face.EigenFaceRecognizer_create()
     if modelo == 'FisherFaces': recognizer = cv.face.FisherFaceRecognizer_create()
     if modelo == 'LBPH': recognizer = cv.face.LBPHFaceRecognizer_create()
 
-    path = 'users/' + user_id + "/recognizer/trainer.yml"
+    path = 'users/' + user + "/recognizer/trainer.yml"
 
     recognizer.read(path)
 
-    labels = {user_id : 0}
+    labels = {user : 0}
 
-    id_, conf = recognizer.predict(face)
+    id_, conf = recognizer.predict(face) # TODO mirar en la documentacion pero se puede hacer que esto te devuelva un % con la confianza que reconoce
 
     if conf > 0:
         print("Acierto")
+        return True
     
     else:
         print("Fallo")
+        return False
 
 
 def detectMainFace(frame):
@@ -98,8 +93,9 @@ def detectMainFace(frame):
 
     for (x,y,w,h) in faces:
 
-        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        faceROI = frame_gray[y:y+h,x:x+w]
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # TODO: esto creo que debería borrarlo 
+        faceROI = frame_gray[y:y+h,x:x+w]                       # y esto no se ni que hace
+        
 
         if (biggerFace["w"] * biggerFace["h"]) < (w * h):   # guardar solo la cara más grande (ver a ver si deberíamos coger las 2 caras mas grandes en caso de que las dos estén cerca de tamaño)
             biggerFace["x"], biggerFace["y"], biggerFace["w"], biggerFace["h"] = x, y, w, h
@@ -112,6 +108,7 @@ def detectMainFace(frame):
     # cv.imshow('Capture - Face detection', mainFace)
     # cv.waitKey(0)
 
+    faceDimentions = (260,260)
     mainFace = cv.resize(mainFace, faceDimentions)
 
     return mainFace
@@ -119,14 +116,14 @@ def detectMainFace(frame):
 
 def saveFace(face, identifier):
 
-    path = "users/" + user_id + '/train/'
+    path = "users/" + identifier + '/train/'
     
     id = len(os.listdir(path)) + 1
 
     cv.imwrite(path + str(id) + ".jpg", face) 
 
 
-def captureAndSaveFaces():
+def captureAndSaveFaces(user):
 
     path = 'PruebasOpenCV\images\\'
 
@@ -134,7 +131,7 @@ def captureAndSaveFaces():
         imagePath = path + img
         img = cv.imread(imagePath)
         mainFace = detectMainFace(img)
-        saveFace(mainFace, user_id)
+        saveFace(mainFace, user)
 
     """cap = cv.VideoCapture(0) 
 
@@ -147,17 +144,17 @@ def captureAndSaveFaces():
         img = cv.flip(img, 1, 0)
         #el código de atuch enseña la imagen y la imprime con rectángulos en la cara detectada
         mainFace = detectMainFace(img)
-        saveFace(mainFace, user_id)"""
+        saveFace(mainFace, user)"""
 
 
-def prepareFolders():
+def prepareFolders(user):
 
     path = "users/"
 
     if not os.path.isdir(path):
         os.mkdir(path)
 
-    path += '/' + user_id
+    path += '/' + user
 
     if not os.path.isdir(path):
         os.mkdir(path)
@@ -179,7 +176,6 @@ def prepareFolders():
 
 
 # Iniciar sesion o registrate. Esto dejarlo para el final, para ahora hacer las pruebas mas rapido
-
 def initUser():
 
     print("\n MENÚ DE INICIO \n")
@@ -222,14 +218,20 @@ def logIn():
         user = input("\nInserte su nombre o DNI: ").lower() 
         userPath = "users" + user + "/"
 
-    global user_id
-    user_id = user
 
     # TODO: sacar fotos y utilizar el modelo entrenado para ver si da acierto o nó
 
     img = cv.imread("PruebasOpenCV\\recognize\scarlett_johannson.jpg")
     face = detectMainFace(img)
-    recognize(user_id, face)
+    
+    if not(recognize(user, face, 'EigenFaces', user)):
+        print("fallo")
+        user = ""
+
+    else:
+        print("acierto")
+
+    return user
 
 
 def register():
@@ -244,84 +246,27 @@ def register():
         user = input("\nInserte su nombre o DNI: ").lower() 
         userPath = 'users/' + user + "/"
 
-    global user_id
-    user_id = user
-        
     #Creamos las carpetas correspondientes
-    prepareFolders()
+    prepareFolders(user)
 
     # Iniciamos la captura de cara
     # faceCapture(userPath, user)
     # TODO: hacer toda la parte de sacarle fotos, capturar la cara y entrenar un modelo con esas fotos
 
     #capturar las caras y guardarlas
-    captureAndSaveFaces()
+    captureAndSaveFaces(user)
 
-    images, labels = getTrainingInfo() #TODO: getTrainingInfo no funciona bien por los paths
-    train(images, labels)
+    images, labels = getTrainingInfo(user) #TODO: getTrainingInfo no funciona bien por los paths
+    train(images, labels, 'EigenFaces', user)
     
     print("Correcto")
 
-
-#TODO: modificar esta función para que funcione hehe que es un copia pega de atuch
-def testearModelo(recognizer):
-
-    if modelo == 'EigenFaces': recognizer.read("modeloEigenFaces.xml")
-    if modelo == 'FisherFaces': recognizer.read("modeloFisherFaces.xml")
-    if modelo == 'LBPH': recognizer.read("modeloLBPH.xml")
-
-
-    # etiquetado y lectura de los nombres de cada identidad
-    #labels = ['Unknown' ,'Scarlett Johansson' , 'Atuch']
-    nombres = ['Scarlett Johansson'] #solo hemos entrenado para ella, por tanto id_ siempre será 0
-
-    # se inicializan los contadores de aciertos, fallos y suma_conf a 0
-    aciertos = 0
-    fallos = 0
-    suma_conf = 0
-    userPathAux = userPath + testPath
-    
-    face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
-    
-    for file in os.listdir(userPathAux): # para cada imagénes del directorio seleccionadas (bbdd de test)
-        
-        if file.endswith("jpg"): # se lee el archivo si acaba en .jpg (formato de las imagénes)
-            subPath = userPathAux + file
-            image = Image.open(subPath).convert("L") # mode L: 8-bit pixels, black and white -> una capa en blanco y negro
-            image_array = np.array(image, "uint8") # convierte imágenes en array de números, unsigned integer (0 a 255)
-            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.2, minNeighbors=4) # detección de caras
-            
-            for (x, y, w, h) in faces: # para cada cara en esa imagen
-                region_of_interest_gray = cv.resize(image_array[y:y+h, x:x+w], (250, 250)) # región de interés de la imagen
-                id_, conf = recognizer.predict(region_of_interest_gray) # predicción de la identidad
-                suma_conf = suma_conf + conf
-
-                if conf < 40:   # (conf<40) (conf>0) JUGAR CON LOS VALORES
-                    persona = "Known"
-                    #if file == nombres[id_]:   #pensar otra condicion
-                    #    print("---  ACIERTO  ---")
-                    #    aciertos += 1
-                    #else:
-                    #    fallos += 1                
-                    #print("Imagen bajo test:", file, "  Prediccion:", nombres[id_], "  Confidence:", conf)
-                else:
-                    persona = "Unknown"
-                
-                print('{}{:>7}{:>20}{:>9}{:>20}{:>6}'.format("Imagen bajo test:", file, "  Prediccion:", persona, "  Confidence:", round(conf,2)))
-
-    # se muestran los resultados en el terminal
-    #print("\nEl numero de aciertos es:", aciertos)
-    #print("El numero de fallos es:", fallos)
-    #print("El numero de imagenes predecidas es:", (aciertos+fallos))
-    #print("El porcentaje de exitos es:", ((aciertos/(aciertos+fallos))*100), "%")
-    #print("La media de confidence es:", (suma_conf/(aciertos+fallos)))
-    
-    #return
+    return user
 
 
 def main():
 
-    prepareFolders()
+    prepareFolders("default")
 
     initUser()
 
