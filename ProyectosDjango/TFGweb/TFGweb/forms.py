f5edaf9b1c3132b0c9cfcc80cbca55544e262737
Adrián Turiel Charro <60858106+atuch17@users.noreply.github.com>
2022-05-27 13:13:48 +0200

    centrando "confirmCreateRecog..."

--- ProyectosDjango/TFGweb/TFGweb/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    """Formulario para autenticar usuarios"""
-    username = forms.CharField(label='Nombre de usuario', max_length=100) 
-    password = forms.CharField(label='Contraseña', max_length=100, widget=forms.PasswordInput)
